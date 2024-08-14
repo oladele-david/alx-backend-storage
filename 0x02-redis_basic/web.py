2753e71dfb27ea950bd085dfce29b16bd73eadac
oladele.david <tdboy59@gmail.com>
2024-08-14 16:51:23 +0100

    radis tasks

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+"""A module with tools for request caching and tracking.
+"""
 import redis
 import requests
 from functools import wraps
@@ -8,17 +8,17 @@ from typing import Callable
 
 
 redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
+"""The module-level Redis instance.
+"""
 
 
 def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
+    """Caches the output of fetched data.
+    """
     @wraps(method)
     def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
+        """The wrapper function for caching the output.
+        """
         redis_store.incr(f'count:{url}')
         result = redis_store.get(f'result:{url}')
         if result:
@@ -32,7 +32,7 @@ def data_cacher(method: Callable) -> Callable:
 
 @data_cacher
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
+    """Returns the content of a URL after caching the request's response,
     and tracking the request.
-    '''
+    """
     return requests.get(url).text
